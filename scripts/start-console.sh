0627bd6e356175c6406018d4262a7f55d5d56207
yzamir <yzamir@redhat.com>
2022-08-16 08:32:33 +0300

    Add initial plugin infrastructure

---
+++ scripts/start-console.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+PLUGIN_NAME="forklift-console-plugin"
+CONSOLE_IMAGE=${CONSOLE_IMAGE:="quay.io/openshift/origin-console:latest"}
+CONSOLE_PORT=${CONSOLE_PORT:=9000}
+INVENTORY_SERVER_HOST=${INVENTORY_SERVER_HOST:="http://localhost:8080"}
+MUST_GATHER_API_SERVER_HOST=${MUST_GATHER_API_SERVER_HOST:="http://localhost:9200"}
+
+echo "Starting local OpenShift console..."
+
+BRIDGE_USER_AUTH="disabled"
+BRIDGE_K8S_MODE="off-cluster"
+BRIDGE_K8S_AUTH="bearer-token"
+BRIDGE_K8S_MODE_OFF_CLUSTER_SKIP_VERIFY_TLS=true
+BRIDGE_K8S_MODE_OFF_CLUSTER_ENDPOINT=$(oc whoami --show-server)
+BRIDGE_K8S_MODE_OFF_CLUSTER_THANOS=$(oc -n openshift-config-managed get configmap monitoring-shared-config -o jsonpath='{.data.thanosPublicURL}')
+BRIDGE_K8S_MODE_OFF_CLUSTER_ALERTMANAGER=$(oc -n openshift-config-managed get configmap monitoring-shared-config -o jsonpath='{.data.alertmanagerPublicURL}')
+BRIDGE_K8S_AUTH_BEARER_TOKEN=$(oc whoami --show-token 2>/dev/null)
+BRIDGE_USER_SETTINGS_LOCATION="localstorage"
+
+echo "API Server: $BRIDGE_K8S_MODE_OFF_CLUSTER_ENDPOINT"
+echo "Console Image: $CONSOLE_IMAGE"
+echo "Console URL: http://localhost:${CONSOLE_PORT}"
+echo "Inventory server URL: ${INVENTORY_SERVER_HOST}"
+echo "Must gather API server URL: ${MUST_GATHER_API_SERVER_HOST}"
+
+# Look for a forklift-inventory route
+PLUGIN_PROXY=$(cat << END
+{"services":[
+    {
+        "consoleAPIPath":"/api/proxy/plugin/forklift-console-plugin/forklift-inventory/",
+        "endpoint":"${INVENTORY_SERVER_HOST}",
+        "authorize":true
+    },
+    {
+        "consoleAPIPath":"/api/proxy/plugin/forklift-console-plugin/must-gather-api/",
+        "endpoint":"${MUST_GATHER_API_SERVER_HOST}",
+        "authorize":true
+    }]}
+END
+)
+BRIDGE_PLUGIN_PROXY=$(echo ${PLUGIN_PROXY} | sed 's/[ \n]//g')
+
+# Prefer podman if installed. Otherwise, fall back to docker.
+if [ -x "$(command -v podman)" ]; then
+    if [ "$(uname -s)" = "Linux" ]; then
+        # Use host networking on Linux since host.containers.internal is unreachable in some environments.
+        BRIDGE_PLUGINS="${PLUGIN_NAME}=http://localhost:9001"
+        podman run --pull always --rm --network=host --env-file <(set | grep BRIDGE | sed "s/'//g") $CONSOLE_IMAGE
+    else
+        BRIDGE_PLUGINS="${PLUGIN_NAME}=http://host.containers.internal:9001"
+        podman run --pull always --rm -p "$CONSOLE_PORT":9000 --env-file <(set | grep BRIDGE | sed "s/'//g") $CONSOLE_IMAGE
+    fi
+else
+    BRIDGE_PLUGINS="${PLUGIN_NAME}=http://host.docker.internal:9001"
+    docker run --pull always --rm -p "$CONSOLE_PORT":9000 --env-file <(set | grep BRIDGE | sed "s/'//g") $CONSOLE_IMAGE
+fi
---
+++ webpack.config.ts
@@ -0,0 +1,103 @@
+/* eslint-env node */
+
+import * as path from 'path';
+
+import CopyPlugin from 'copy-webpack-plugin';
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+
+import { ConsoleRemotePlugin } from '@openshift-console/dynamic-plugin-sdk-webpack';
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const config: Configuration = {
+  mode: 'development',
+  // No regular entry points. The remote container entry is handled by ConsoleRemotePlugin.
+  entry: {},
+  context: path.resolve(__dirname, 'src'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]-bundle.js',
+    chunkFilename: '[name]-chunk.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?|tsx?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s?(css)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff2?|ttf|eot|otf)(\?.*$|$)/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[name].[ext]',
+        },
+      },
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+    port: 9001,
+    // Allow bridge running in a container to connect to the plugin dev server.
+    allowedHosts: 'all',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, Content-Type, Authorization',
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+  plugins: [
+    new ConsoleRemotePlugin(),
+    new CopyPlugin({
+      patterns: [{ from: '../locales', to: '../dist/locales' }],
+    }),
+  ],
+  devtool: 'source-map',
+  optimization: {
+    chunkIds: 'named',
+    minimize: false,
+  },
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.mode = 'production';
+  if (config.output !== undefined) {
+    config.output.filename = '[name]-bundle-[hash].min.js';
+    config.output.chunkFilename = '[name]-chunk-[chunkhash].min.js';
+  }
+  if (config.optimization !== undefined) {
+    config.optimization.chunkIds = 'deterministic';
+    config.optimization.minimize = true;
+  }
+}
+
+export default config;
