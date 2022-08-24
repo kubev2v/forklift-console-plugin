35c1c8802d69fbea8485cc4cf1c71b47b4b69e01
yzamir <yzamir@redhat.com>
2022-08-24 14:24:18 +0300

    Move the minikube ip and token to the minkube run script

--- scripts/start-console-minikube.sh
+++ scripts/start-console-minikube.sh
@@ -2,9 +2,16 @@
 
 set -euo pipefail
 
+# Set minikube IP and token (see scripts/configure-minikube.shcut)
+export BRIDGE_K8S_MODE_OFF_CLUSTER_ENDPOINT=${BRIDGE_K8S_MODE_OFF_CLUSTER_ENDPOINT:=https://$(minikube ip):8443}
+
+# Default minkube home to user home, if MINIKUBE_HOME is not defined
+MINIKUBE_HOME_=${MINIKUBE_HOME:=$HOME}
+token=$(cat ${MINIKUBE_HOME_}/.minikube/files/etc/ca-certificates/token.csv 2>/dev/null | cut -f 1 -d,)
+export BRIDGE_K8S_AUTH_BEARER_TOKEN=${BRIDGE_K8S_AUTH_BEARER_TOKEN:=${token}}
+
 # Run console
 # -----------
-
 
 PLUGIN_NAME="forklift-console-plugin"
 CONSOLE_IMAGE=${CONSOLE_IMAGE:="quay.io/openshift/origin-console:latest"}
