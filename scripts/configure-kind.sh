16c5ea9e629c3ebedc1860ba95a3decce0c9b2a7
yzamir <yzamir@redhat.com>
2022-08-31 15:49:52 +0300

    Use generic k8s commands to get the api server URL

---
+++ scripts/configure-kind.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Start kind
+# ------------
+
+# Hint - install kind [ linux-amd64 ]
+#curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+#sudo install kind-linux-amd64 /usr/local/bin/kind
+
+echo "Starting local kind cluster..."
+
+# Start kind
+kind create cluster --config $(pwd)/deployment/hack/kind/config.yaml
+
+# Make default user super tux on the cluster
+kubectl create clusterrolebinding kind-admin --clusterrole=cluster-admin --user=system:bootstrap:abcdef
+
+# Try to install forklift CRDs
+kubectl apply -f $(pwd)/deployment/hack/crds
