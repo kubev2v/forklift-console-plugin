19e7228689da50a7260603385c36ce43dd490ae6
yzamir <yzamir@redhat.com>
2022-11-17 12:25:38 +0200

    Consolidate plugin extension modules
    Signed-off-by: yzamir <yzamir@redhat.com>

--- console-extensions.ts
+++ console-extensions.ts
@@ -1,11 +1,9 @@
 import type { EncodedExtension } from '@openshift/dynamic-plugin-sdk';
-import type { HrefNavItem, NavSection, Separator } from '@openshift-console/dynamic-plugin-sdk';
+import type { NavSection, Separator } from '@openshift-console/dynamic-plugin-sdk';
 
-import { extensions as hostExtensions } from './src/modules/Hosts/dynamic-plugin';
 import { extensions as mappingExtensions } from './src/modules/Mappings/dynamic-plugin';
 import { extensions as planExtensions } from './src/modules/Plans/dynamic-plugin';
 import { extensions as providerExtensions } from './src/modules/Providers/dynamic-plugin';
-import { extensions as vmMigrationDetailExtensions } from './src/modules/VmMigrationDetails/dynamic-plugin';
 
 const extensions: EncodedExtension[] = [
   {
@@ -38,46 +36,9 @@ const extensions: EncodedExtension[] = [
     },
   } as EncodedExtension<Separator>,
 
-  {
-    type: 'console.navigation/href',
-    properties: {
-      id: 'providers',
-      insertAfter: 'importSeparator',
-      perspective: 'admin',
-      section: 'virtualization',
-      name: '%plugin__forklift-console-plugin~Providers for VM Import%',
-      href: '/mtv/providers',
-    },
-  } as EncodedExtension<HrefNavItem>,
-
-  {
-    type: 'console.navigation/href',
-    properties: {
-      id: 'plans',
-      insertAfter: 'providers',
-      section: 'virtualization',
-      name: '%plugin__forklift-console-plugin~Plans for VM Import%',
-      href: '/mtv/plans',
-    },
-  } as EncodedExtension<HrefNavItem>,
-
-  {
-    type: 'console.navigation/href',
-    properties: {
-      id: 'mappings',
-      insertAfter: 'plans',
-      perspective: 'admin',
-      section: 'virtualization',
-      name: '%plugin__forklift-console-plugin~Mappings for VM Import%',
-      href: '/mtv/mappings',
-    },
-  } as EncodedExtension<HrefNavItem>,
-
   ...providerExtensions,
-  ...hostExtensions,
   ...planExtensions,
   ...mappingExtensions,
-  ...vmMigrationDetailExtensions,
 ];
 
 export default extensions;
