bd9c108d9b6b3fef0ea9e17836be50dd2e40a679
yzamir <yzamir@redhat.com>
2023-02-07 09:22:15 +0200

    Update the i18n package path in the i18n test
    Signed-off-by: yzamir <yzamir@redhat.com>

--- ci/test-i18n.sh
+++ ci/test-i18n.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 npm run i18n
-GIT_STATUS="$(git status --short --untracked-files -- locales)"
+GIT_STATUS="$(git status --short --untracked-files -- packages/forklift-console-plugin/locales)"
 if [ -n "$GIT_STATUS" ]; then
   echo "i18n files are not up to date. Commit them to fix."
   git diff
