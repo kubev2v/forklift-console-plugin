cd7ca9aa990832826892590ccbc01a6957de7ad3
yzamir <yzamir@redhat.com>
2023-02-13 15:42:58 +0200

    Add auto generated files
    Signed-off-by: yzamir <yzamir@redhat.com>

---
+++ packages/types/src/constants/OvirtVolumePopulatorModel.v1beta1.ts
@@ -0,0 +1,28 @@
+// Auto generated from k8s CRD file
+//  see: https://github.com/yaacov/crdtoapi
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: this tool has poor color perception, you are encouraged to change the color and abbr attributes.
+
+export const OvirtVolumePopulatorModel = {
+  label: 'OvirtVolumePopulator',
+  labelPlural: 'OvirtVolumePopulators',
+
+  apiVersion: 'v1beta1',
+  apiGroup: 'forklift.konveyor.io',
+  kind: 'OvirtVolumePopulator',
+  plural: 'ovirtvolumepopulators',
+
+  abbr: 'OV',
+  color: undefined,
+  id: 'ovirtvolumepopulators.forklift.konveyor.io',
+  namespaced: true,
+  crd: true,
+};
+
+export const OvirtVolumePopulatorModelGroupVersionKind = {
+  version: 'v1beta1',
+  kind: 'OvirtVolumePopulator',
+  group: 'forklift.konveyor.io',
+};
+export const OvirtVolumePopulatorModelRef = 'forklift.konveyor.io~v1beta1~OvirtVolumePopulator';
---
+++ packages/types/src/constants/PlanModel.v1beta1.ts
@@ -0,0 +1,28 @@
+// Auto generated from k8s CRD file
+//  see: https://github.com/yaacov/crdtoapi
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: this tool has poor color perception, you are encouraged to change the color and abbr attributes.
+
+export const PlanModel = {
+  label: 'Plan',
+  labelPlural: 'Plans',
+
+  apiVersion: 'v1beta1',
+  apiGroup: 'forklift.konveyor.io',
+  kind: 'Plan',
+  plural: 'plans',
+
+  abbr: 'PL',
+  color: undefined,
+  id: 'plans.forklift.konveyor.io',
+  namespaced: true,
+  crd: true,
+};
+
+export const PlanModelGroupVersionKind = {
+  version: 'v1beta1',
+  kind: 'Plan',
+  group: 'forklift.konveyor.io',
+};
+export const PlanModelRef = 'forklift.konveyor.io~v1beta1~Plan';
