385ed51337e3d4b2ada0b8d865981b14d6b134a8
yzamir <yzamir@redhat.com>
2023-03-27 10:34:57 +0300

    Remove dev build and helm
    Signed-off-by: yzamir <yzamir@redhat.com>

--- ci/patch-vcsim-service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-FORKLIFT_NAMESPACE=konveyor-forklift
-kubectl patch service -n ${FORKLIFT_NAMESPACE} vcsim --type='merge' \
-  -p '{"spec":{"type":"NodePort","ports":[{"name":"vcsim","targetPort":8989,"port":8989,"nodePort":30089}]}}'
