6f1b4750ea2b836c2f5eaa58f5777e66212f720f
yzamir <yzamir@redhat.com>
2023-03-27 10:56:29 +0300

    Consolidate console yaml for e2e tests
    Signed-off-by: yzamir <yzamir@redhat.com>

--- ci/deploy-console.sh
+++ ci/deploy-console.sh
@@ -6,6 +6,9 @@ script_dir=$(dirname "$0")
 K8S_TIMEOUT=${K8S_TIMEOUT:="360s"}
 OKD_CONSOLE_YAML=${script_dir}/yaml/okd-console.yaml
 
+FORKLIFT_PLUGIN_UPSTREAM_IMG=quay.io/kubev2v/forklift-console-plugin:latest
+FORKLIFT_PLUGIN_LOCAL_IMG=localhost:5001/forklift-console-plugin:latest
+
 # Install OKD console
 # -------------------
 echo ""
@@ -16,14 +19,23 @@ echo ""
 echo "deploy console CRDs"
 
 kubectl apply -f ${script_dir}/yaml/crds/console
+kubectl apply -f ${script_dir}/yaml/crds/forklift
 
 echo ""
 echo "deploy OKD console (port: 30088)"
 
-kubectl apply -f ${OKD_CONSOLE_YAML}
+cat ${OKD_CONSOLE_YAML} | \
+    sed "s/${FORKLIFT_PLUGIN_UPSTREAM_IMG//\//\\/}/${FORKLIFT_PLUGIN_LOCAL_IMG//\//\\/}/g" | \
+    kubectl apply -f -
 
 echo ""
 echo "waiting for OKD console service..."
 echo "=================================="
 
 kubectl wait deployment -n okd-console console --for condition=Available=True --timeout=${K8S_TIMEOUT}
+
+echo ""
+echo "waiting for forklift console plugin service..."
+echo "========================================="
+
+kubectl wait deployment -n konveyor-forklift forklift-console-plugin --for condition=Available=True --timeout=${K8S_TIMEOUT}
