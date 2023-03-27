ef0e14e197e18ab31adfae91d52956cc8cf1caa7
yzamir <yzamir@redhat.com>
2023-03-27 11:35:29 +0300

    Remove docker support
    Signed-off-by: yzamir <yzamir@redhat.com>

--- ci/deploy-cluster.sh
+++ ci/deploy-cluster.sh
@@ -15,27 +15,21 @@ bash ${script_dir}/install-kubectl.sh
 reg_name='kind-registry'
 reg_port='5001'
 
-if [ -x "$(command -v docker)" ]; then
-  if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
-    docker run \
-      -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
-      registry:2
-  fi
-else
-  # Create the kind network
-  if ! podman network exists kind; then
-    podman network create kind
-  fi
-
-  # Create the registry
-  if [ "$(podman inspect -f {{.State.Running}} "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
-    podman run \
-      -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" --net kind \
-      registry:2
-  fi
+if ! podman network exists kind; then
+  podman network create kind -d bridge
 fi
 
+# Create the registry
+if [ "$(podman inspect -f {{.State.Running}} "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
+  podman run \
+    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" --net kind \
+    registry:2
+fi
+reg_ip=$(podman inspect -f {{.NetworkSettings.Networks.kind.IPAddress}} ${reg_name})
+
 # create a cluster with the local registry enabled in containerd
+KIND_EXPERIMENTAL_PROVIDER=podman
+
 cat <<EOF | kind create cluster --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
@@ -51,15 +45,8 @@ nodes:
 containerdConfigPatches:
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
-    endpoint = ["http://${reg_name}:5000"]
+    endpoint = ["http://${reg_ip}:5000"]
 EOF
-
-if [ -x "$(command -v docker)" ]; then
-  # connect the registry to the cluster network if not already connected
-  if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
-    docker network connect "kind" "${reg_name}"
-  fi
-fi
 
 echo ""
 echo "Bind cluster admin role to default user"
