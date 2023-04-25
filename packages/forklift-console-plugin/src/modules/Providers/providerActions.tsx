8b8fb45144dd452ce69ce606df68d0d571b9fa98
Radoslaw Szwajkowski <rszwajko@redhat.com>
2023-04-25 18:57:38 +0200

    Revert dropping 'Select migration network' action
    Fix regression introduced in #289. Backported from #409.
    
    Reference-Url: https://github.com/kubev2v/forklift-console-plugin/pull/289
    Reference-Url: https://github.com/kubev2v/forklift-console-plugin/pull/409
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- packages/forklift-console-plugin/src/modules/Providers/providerActions.tsx
+++ packages/forklift-console-plugin/src/modules/Providers/providerActions.tsx
@@ -1,16 +1,22 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import { Trans } from 'react-i18next';
+import { DEFAULT_TRANSFER_NETWORK_ANNOTATION } from 'src/utils/constants';
 import { useTranslation } from 'src/utils/i18n';
 
 import { withActionServiceContext } from '@kubev2v/common/components/ActionServiceDropdown';
 import withQueryClient from '@kubev2v/common/components/QueryClientHoc';
 import { useModal } from '@kubev2v/common/polyfills/sdk-shim';
 import { ConfirmModal } from '@kubev2v/legacy/common/components/ConfirmModal';
+import { SelectOpenShiftNetworkModal } from '@kubev2v/legacy/common/components/SelectOpenShiftNetworkModal';
 import { ProviderType } from '@kubev2v/legacy/common/constants';
 import { AddEditProviderModal } from '@kubev2v/legacy/Providers/components/AddEditProviderModal';
 import { hasRunningMigration } from '@kubev2v/legacy/Providers/components/ProvidersTable';
-import { useDeleteProviderMutation, usePlansQuery } from '@kubev2v/legacy/queries';
-import { IProviderObject } from '@kubev2v/legacy/queries/types';
+import {
+  useDeleteProviderMutation,
+  useOCPMigrationNetworkMutation,
+  usePlansQuery,
+} from '@kubev2v/legacy/queries';
+import { IOpenShiftProvider, IProviderObject } from '@kubev2v/legacy/queries/types';
 
 import { type MergedProvider, isManaged } from './data';
 
@@ -37,23 +43,29 @@ export const useMergedProviderActions = ({ resourceData }: { resourceData: Merge
     : t('This provider cannot be deleted because it has running migrations');
 
   const actions = useMemo(
-    () => [
-      {
-        id: 'edit',
-        cta: () => launchModal(withQueryClient(EditModal), { resourceData }),
-        label: t('Edit Provider'),
-        disabled: editingDisabled,
-        disabledTooltip: editingDisabled ? editDisabledTooltip : '',
-      },
-      {
-        id: 'delete',
-        cta: () => launchModal(withQueryClient(DeleteModal), { resourceData }),
-        label: t('Delete Provider'),
-        disabled: editingDisabled,
-        disabledTooltip: editingDisabled ? deleteDisabledTooltip : '',
-      },
-    ],
-    [t, editingDisabled, resourceData],
+    () =>
+      [
+        {
+          id: 'edit',
+          cta: () => launchModal(withQueryClient(EditModal), { resourceData }),
+          label: t('Edit Provider'),
+          disabled: editingDisabled,
+          disabledTooltip: editingDisabled ? editDisabledTooltip : '',
+        },
+        {
+          id: 'delete',
+          cta: () => launchModal(withQueryClient(DeleteModal), { resourceData }),
+          label: t('Delete Provider'),
+          disabled: editingDisabled,
+          disabledTooltip: editingDisabled ? deleteDisabledTooltip : '',
+        },
+        resourceData.spec?.type === 'openshift' && {
+          id: 'selectNetwork',
+          cta: () => launchModal(withQueryClient(SelectNetworkForOpenshift), { resourceData }),
+          label: t('Select migration network'),
+        },
+      ].filter(Boolean),
+    [t, editingDisabled, editDisabledTooltip, deleteDisabledTooltip, resourceData],
   );
 
   return [actions, true, undefined];
@@ -75,6 +87,44 @@ const EditModal = ({
   );
 };
 EditModal.displayName = 'EditModal';
+
+const SelectNetworkForOpenshift = ({
+  resourceData,
+  closeModal,
+}: {
+  closeModal: () => void;
+  resourceData: MergedProvider;
+}) => {
+  const { t } = useTranslation();
+  const migrationNetworkMutation = useOCPMigrationNetworkMutation(
+    resourceData.metadata?.namespace,
+    closeModal,
+  );
+  return (
+    <SelectOpenShiftNetworkModal
+      targetProvider={resourceData.inventory as IOpenShiftProvider}
+      targetNamespace={null}
+      initialSelectedNetwork={
+        resourceData.metadata?.annotations?.[DEFAULT_TRANSFER_NETWORK_ANNOTATION]
+      }
+      instructions={t(
+        'Select a default migration network for the provider. This network will be used for migrating data to all namespaces to which it is attached.',
+      )}
+      onClose={() => {
+        migrationNetworkMutation.reset();
+        closeModal();
+      }}
+      onSubmit={(network) =>
+        migrationNetworkMutation.mutate({
+          provider: resourceData.inventory as IOpenShiftProvider,
+          network,
+        })
+      }
+      mutationResult={migrationNetworkMutation}
+    />
+  );
+};
+SelectNetworkForOpenshift.displayName = 'SelectNetworkForOpenshift';
 
 const DeleteModal = ({
   resourceData,
