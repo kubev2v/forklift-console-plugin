591acd4332545968208b560899dcfd84443af20a
yzamir <yzamir@redhat.com>
2023-04-27 15:57:30 +0300

    Update inventory types
    Signed-off-by: yzamir <yzamir@redhat.com>

---
+++ packages/types/src/types/OVirtStorageDomains.ts
@@ -0,0 +1,13 @@
+/** Represents an oVirt storage domain. */
+export interface OVirtStorageDomains {
+  /** The unique identifier of the storage domain. */
+  id: string;
+  /** The revision number of the storage domain. */
+  revision: number;
+  /** The display name of the storage domain. */
+  name: string;
+  /** The self link of the storage domain. */
+  selfLink: string;
+  /** The path of the storage domain (optional). */
+  path?: string | null;
+}
---
+++ packages/types/src/types/VSphereHostInventory.ts
@@ -0,0 +1,139 @@
+/**
+ * Represents the inventory of a vSphere host.
+ */
+export interface VSphereHostInventory {
+  /** The unique identifier of the host. */
+  id: string;
+  /** The parent object of the host, which could be a network, datastore, or another parent object. */
+  parent: NetworksEntityOrDatastoresEntityOrParent;
+  /** The path of the host in the vSphere inventory tree. */
+  path: string;
+  /** The revision number of the host. */
+  revision: number;
+  /** The name of the host. */
+  name: string;
+  /** The self-link of the host. */
+  selfLink: string;
+  /** The name of the cluster the host belongs to. */
+  cluster: string;
+  /** The status of the host. */
+  status: string;
+  /** Indicates whether the host is in maintenance mode. */
+  inMaintenance: boolean;
+  /** The IP address of the management server for the host. */
+  managementServerIp: string;
+  /** The thumbprint of the host's SSL certificate. */
+  thumbprint: string;
+  /** The time zone of the host. */
+  timezone: string;
+  /** The number of CPU sockets on the host. */
+  cpuSockets: number;
+  /** The number of CPU cores on the host. */
+  cpuCores: number;
+  /** The name of the product running on the host. */
+  productName: string;
+  /** The version of the product running on the host. */
+  productVersion: string;
+  /** The networking configuration for the host. */
+  networking: Networking;
+  /** The list of networks available on the host. */
+  networks?: NetworksEntityOrDatastoresEntityOrParent[] | null;
+  /** The list of datastores available on the host. */
+  datastores?: NetworksEntityOrDatastoresEntityOrParent[] | null;
+  /** The list of network adapters on the host. */
+  networkAdapters?: NetworkAdaptersEntity[] | null;
+}
+
+/**
+ * Represents an entity that can be a network, datastore, or parent object.
+ */
+export interface NetworksEntityOrDatastoresEntityOrParent {
+  /** The type of entity. */
+  kind: string;
+  /** The unique identifier of the entity. */
+  id: string;
+}
+
+/**
+ * Represents the networking configuration for a vSphere host.
+ */
+export interface Networking {
+  /** The list of physical NICs on the host. */
+  pNICs?: PNICsEntity[] | null;
+  /** The list of virtual NICs on the host. */
+  vNICs?: VNICsEntity[] | null;
+  /** The list of port groups on the host. */
+  portGroups?: PortGroupsEntity[] | null;
+  /** The list of switches on the host. */
+  switches?: SwitchesEntity[] | null;
+}
+
+/**
+ * Represents a physical NIC on a vSphere host.
+ */
+export interface PNICsEntity {
+  /** The unique identifier of the physical NIC. */
+  key: string;
+  /** The link speed of the physical NIC. */
+  linkSpeed: number;
+}
+
+/**
+ * Represents a virtual NIC on a vSphere host.
+ */
+export interface VNICsEntity {
+  /** The unique identifier of the virtual NIC. */
+  key: string;
+  /** The port group the virtual NIC is connected to. */
+  portGroup: string;
+  /** The distributed port group the virtual NIC is connected to. */
+  dPortGroup: string;
+  /** The IP address of the virtual NIC. */
+  ipAddress: string;
+  /** The subnet mask of the virtual NIC. */
+  subnetMask: string;
+  /** The maximum transmission unit (MTU) of the virtual NIC. */
+  mtu: number;
+}
+
+/**
+ * Represents a port group on a vSphere host.
+ */
+export interface PortGroupsEntity {
+  /** The unique identifier of the port group. */
+  key: string;
+  /** The name of the port group. */
+  name: string;
+  /** The virtual switch the port group is connected to. */
+  vSwitch: string;
+}
+
+/**
+ * Represents a switch on a vSphere host.
+ */
+export interface SwitchesEntity {
+  /** The unique identifier of the switch. */
+  key: string;
+  /** The name of the switch. */
+  name: string;
+  /** The list of port groups on the switch. */
+  portGroups?: string[] | null;
+  /** The list of physical NICs connected to the switch. */
+  pNICs?: string[] | null;
+}
+
+/**
+ * Represents a network adapter on a vSphere host.
+ */
+export interface NetworkAdaptersEntity {
+  /** The name of the network adapter. */
+  name: string;
+  /** The IP address of the network adapter. */
+  ipAddress: string;
+  /** The subnet mask of the network adapter. */
+  subnetMask: string;
+  /** The link speed of the network adapter. */
+  linkSpeed: number;
+  /** The maximum transmission unit (MTU) of the network adapter. */
+  mtu: number;
+}
