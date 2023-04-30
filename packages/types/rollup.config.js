d1b0d03e75f026cfbc8416341911df160b07b432
yzamir <yzamir@redhat.com>
2023-04-30 17:12:16 +0300

    Set of fixes for #411
    Signed-off-by: yzamir <yzamir@redhat.com>

--- packages/types/rollup.config.js
+++ packages/types/rollup.config.js
@@ -2,4 +2,4 @@ import { tsLibConfig } from '../../config/rollup-configs';
 
 import pkg from './package.json';
 
-export default tsLibConfig(pkg, 'src/index.ts');
+export default tsLibConfig(pkg, './src/index.ts');
