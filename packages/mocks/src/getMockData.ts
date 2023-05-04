f1b2a73955335c3c8f057c0264598bb079b2c10c
yzamir <yzamir@redhat.com>
2023-05-04 15:08:56 +0300

    Make getMockData method generic
    Signed-off-by: yzamir <yzamir@redhat.com>

--- packages/mocks/src/getMockData.ts
+++ packages/mocks/src/getMockData.ts
@@ -12,24 +12,25 @@ const mockData: Record<string, object> = {
 /**
  * Get a mock response for a given request and path parameters.
  *
+ * @template T - The expected type of the response body (defaults to `object`)
  * @param {Object} param0 - The object containing the request parameters
  * @param {string} param0.pathname - The incoming request pathname to match against the mock handlers
- * @param {string} param0.method - An optional string, the incoming request method to match against the mock handlers
- * @param {PathParams} param0.params - An optional object containing the path parameters of the request
- * @returns {MockResponse | null} A mock response or null if no mock handler is found
+ * @param {string} [param0.method] - An optional string, the incoming request method to match against the mock handlers
+ * @param {PathParams} [param0.params] - An optional object containing the path parameters of the request
+ * @returns {MockResponse<T> | null} A mock response or null if no mock handler is found
  */
-export const getMockData = ({ pathname }: MockDataRequestParameters): MockResponse | null => {
+export const getMockData = <T = object>({
+  pathname,
+}: MockDataRequestParameters): MockResponse<T> | null => {
+  // Dynamic handlers, using custom logic.
+
   // Add your mock handlers here, e.g.:
   // if (method === 'GET' && pathname === '/api/some-resource' && params['details'] === '1') {
   //   return mockResponse;
   // }
 
-  // Dynamic handlers, using custom logic.
-
-  // ...
-
   // Static handlers, using mockData.
-  const data = mockData[pathname] || null;
+  const data = (mockData[pathname] as T) || null;
   if (data) {
     return {
       statusCode: 200,
