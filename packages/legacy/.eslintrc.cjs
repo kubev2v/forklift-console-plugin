2c89c2541799a7cd30de08044f399069fc4ccd17
Scott J Dickerson <sdickers@redhat.com>
2023-06-12 16:01:40 -0400

    Fix eslint on the legacy package
    With commit 62023c6 moving the eslint config file for the legacy
    package from the generic `.eslintrc` json file to `.eslintrc.js`,
    the configuration was broken.  The new js file did not properly
    export the config using `module.export` instead of the correct
    `module.exports`.
    
    Fixing the export to be `module.exports` and adopting the `ignorePatterns`
    and `overrides` sections from #486 and #495 fixes the linting and
    brings the included/excluded files inline with the rest of the
    monorepo's linting.
    
    Linting errors are present (315 errors,4 warnings).  They will be fixed
    in a following commit.
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- packages/legacy/.eslintrc.cjs
+++ packages/legacy/.eslintrc.cjs
@@ -1,61 +1,88 @@
-module.export = {
-  "root": true,
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 2015,
-    "tsx": true,
-    "jsx": true,
-    "js": true,
-    "useJSXTextNode": true,
-    "project": ["tsconfig.json"],
-    "tsconfigRootDir": __dirname,
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2015,
+    tsx: true,
+    jsx: true,
+    js: true,
+    useJSXTextNode: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "prettier"
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
-  "globals": {
-    "window": "readonly",
-    "describe": "readonly",
-    "test": "readonly",
-    "expect": "readonly",
-    "it": "readonly",
-    "process": "readonly",
-    "document": "readonly"
+  globals: {
+    window: 'readonly',
+    describe: 'readonly',
+    test: 'readonly',
+    expect: 'readonly',
+    it: 'readonly',
+    process: 'readonly',
+    document: 'readonly',
   },
-  "settings": {
-    "react": {
-      "version": "^16.11.0"
-    }
+  settings: {
+    react: {
+      version: '^16.11.0',
+    },
   },
-  "plugins": ["prettier", "@typescript-eslint", "react-hooks", "eslint-plugin-react-hooks", "testing-library"],
-  "rules": {
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "@typescript-eslint/interface-name-prefix": "off",
-    "import/no-unresolved": "off",
-    "import/extensions": "off",
-    "react/prop-types": "off",
-    "prettier/prettier": [
-      "error",
+  plugins: [
+    'prettier',
+    '@typescript-eslint',
+    'react-hooks',
+    'eslint-plugin-react-hooks',
+    'testing-library',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': [
+      'error',
       {
-        "singleQuote": true,
-        "semi": true
-      }
+        singleQuote: true,
+        semi: true,
+      },
     ],
-    "testing-library/await-async-query": "error",
-    "testing-library/no-await-sync-query": "error",
-    "testing-library/no-debugging-utils": "warn",
-    "testing-library/no-dom-import": "off"
+    'testing-library/await-async-query': 'error',
+    'testing-library/no-await-sync-query': 'error',
+    'testing-library/no-debugging-utils': 'warn',
+    'testing-library/no-dom-import': 'off',
   },
-  "env": {
-    "browser": true,
-    "node": true
-  }
+  env: {
+    browser: true,
+    node: true,
+  },
+
+  ignorePatterns: [
+    // don't ignore dot files so config files get linted
+    '!.*.js',
+    '!.*.cjs',
+    '!.*.mjs',
+
+    // take the place of `.eslintignore`
+    'dist/',
+    'generated/',
+    'node_modules/',
+  ],
+
+  // this is a hack to make sure eslint will look at all of the file extensions we
+  // care about without having to put it on the command line
+  overrides: [
+    {
+      files: ['**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs', '**/*.ts', '**/*.tsx'],
+    },
+  ],
 };
