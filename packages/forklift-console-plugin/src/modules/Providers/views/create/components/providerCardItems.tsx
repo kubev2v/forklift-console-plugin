583c864aefd774059a1af3791873c75bf3d737e6
yzamir <kobi.zamir@gmail.com>
2023-08-17 16:43:23 +0300

    Reorder types list in create provider form
    Signed-off-by: yzamir <kobi.zamir@gmail.com>

--- packages/forklift-console-plugin/src/modules/Providers/views/create/components/providerCardItems.tsx
+++ packages/forklift-console-plugin/src/modules/Providers/views/create/components/providerCardItems.tsx
@@ -53,29 +53,29 @@ const ovaLogo = (
 );
 
 export const providerCardItems: Record<string, SelectableGalleryItem> = {
-  openshift: {
-    title: 'OpenShift Virtualization',
-    logo: openshiftLogo,
-    content: 'OpenShift Virtualization run and manage virtual machine in OpenShift.',
-  },
-  openstack: {
-    title: 'OpenStack',
-    logo: openstackLogo,
-    content: 'OpenStack is a cloud computing platform that controls large pools of resources.',
+  vsphere: {
+    title: 'vSphere',
+    logo: vmLogo,
+    content: "vSphere is VMware's cloud computing virtualization platform.",
   },
   ovirt: {
     title: 'Red Hat Virtualization',
     logo: redhatLogo,
     content: 'Red Hat Virtualization (RHV) is a virtualization platform from Red Hat.',
   },
-  vsphere: {
-    title: 'vSphere',
-    logo: vmLogo,
-    content: "vSphere is VMware's cloud computing virtualization platform.",
+  openstack: {
+    title: 'OpenStack',
+    logo: openstackLogo,
+    content: 'OpenStack is a cloud computing platform that controls large pools of resources.',
   },
   ova: {
     title: 'Open Virtual Appliance (OVA)',
     logo: ovaLogo,
     content: 'OVA file is a virtual appliance used by virtualization applications.',
+  },
+  openshift: {
+    title: 'OpenShift Virtualization',
+    logo: openshiftLogo,
+    content: 'OpenShift Virtualization run and manage virtual machine in OpenShift.',
   },
 };
