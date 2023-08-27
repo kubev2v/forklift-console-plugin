086f9e9b9d1d8571ff484ab65a0ceea5bddd1704
yzamir <kobi.zamir@gmail.com>
2023-08-27 19:57:31 +0300

    Move warm migration settings to the bottom, and specify main pod in controller pod settings
    Signed-off-by: yzamir <kobi.zamir@gmail.com>

--- packages/forklift-console-plugin/src/modules/Overview/views/overview/tabs/Details/cards/SettingsCard.tsx
+++ packages/forklift-console-plugin/src/modules/Overview/views/overview/tabs/Details/cards/SettingsCard.tsx
@@ -39,6 +39,86 @@ const SettingsCard_: FC<SettingsCardProps> = ({ obj }) => {
           }}
         >
           <DetailsItem
+            title={'Max concurrent virtual machine migrations'}
+            content={
+              obj?.spec?.['controller_max_vm_inflight'] || (
+                <span className="text-muted">{'20'}</span>
+              )
+            }
+            moreInfoLink={
+              'https://access.redhat.com/documentation/en-us/migration_toolkit_for_virtualization/2.4/html-single/installing_and_using_the_migration_toolkit_for_virtualization/index#advanced-migration-options'
+            }
+            helpContent={
+              <Text>
+                {t(
+                  'Sets the maximum number of VMs that can be migrated simultaneously. The default value is 20 virtual machines.',
+                )}
+              </Text>
+            }
+            crumbs={['spec', 'controller_max_vm_inflight']}
+            onEdit={() => showModal(<EditMaxVMInFlightModal resource={obj} />)}
+          />
+
+          <DetailsItem
+            title={'Must gather cleanup after (hours)'}
+            content={mustGatherAPICleanupMaxAge || <span className="text-muted">{'Disabled'}</span>}
+            moreInfoLink={
+              'https://access.redhat.com/documentation/en-us/migration_toolkit_for_virtualization/2.4/html-single/installing_and_using_the_migration_toolkit_for_virtualization/index#advanced-migration-options'
+            }
+            helpContent={
+              <Text>
+                {t(
+                  "Specifies the duration for retaining 'must gather' reports before they are automatically deleted. The default value is -1, which implies automatic cleanup is disabled.",
+                )}
+              </Text>
+            }
+            crumbs={['spec', 'must_gather_api_cleanup_max_age']}
+            onEdit={() => showModal(<EditCleanupMaxAgeModal resource={obj} />)}
+          />
+
+          <DetailsItem
+            title={'Controller main container CPU limit'}
+            content={
+              obj?.spec?.['controller_container_limits_cpu'] || (
+                <span className="text-muted">{'500m'}</span>
+              )
+            }
+            moreInfoLink={
+              'https://access.redhat.com/documentation/en-us/migration_toolkit_for_virtualization/2.4/html-single/installing_and_using_the_migration_toolkit_for_virtualization/index#advanced-migration-options'
+            }
+            helpContent={
+              <Text>
+                {t(
+                  'Defines the CPU limits allocated to the main container in the controller pod. The default value is 500 milliCPU.',
+                )}
+              </Text>
+            }
+            crumbs={['spec', 'controller_container_limits_cpu']}
+            onEdit={() => showModal(<EditControllerCPULimitModal resource={obj} />)}
+          />
+
+          <DetailsItem
+            title={'Controller main container Memory limit'}
+            content={
+              obj?.spec?.['controller_container_limits_memory'] || (
+                <span className="text-muted">{'800Mi'}</span>
+              )
+            }
+            moreInfoLink={
+              'https://access.redhat.com/documentation/en-us/migration_toolkit_for_virtualization/2.4/html-single/installing_and_using_the_migration_toolkit_for_virtualization/index#advanced-migration-options'
+            }
+            helpContent={
+              <Text>
+                {t(
+                  'Sets the memory limits allocated to the main container in the controller pod. The default value is 800Mi.',
+                )}
+              </Text>
+            }
+            crumbs={['spec', 'controller_container_limits_memory']}
+            onEdit={() => showModal(<EditControllerMemoryLimitModal resource={obj} />)}
+          />
+
+          <DetailsItem
             title={'Precopy interval (minutes)'}
             content={
               obj?.spec?.['controller_precopy_interval'] || (
@@ -78,86 +158,6 @@ const SettingsCard_: FC<SettingsCardProps> = ({ obj }) => {
             }
             crumbs={['spec', 'controller_snapshot_status_check_rate_seconds']}
             onEdit={() => showModal(<EditSnapshotPoolingIntervalModal resource={obj} />)}
-          />
-
-          <DetailsItem
-            title={'Max concurrent virtual machine migrations'}
-            content={
-              obj?.spec?.['controller_max_vm_inflight'] || (
-                <span className="text-muted">{'20'}</span>
-              )
-            }
-            moreInfoLink={
-              'https://access.redhat.com/documentation/en-us/migration_toolkit_for_virtualization/2.4/html-single/installing_and_using_the_migration_toolkit_for_virtualization/index#advanced-migration-options'
-            }
-            helpContent={
-              <Text>
-                {t(
-                  'Sets the maximum number of VMs that can be migrated simultaneously. The default value is 20 virtual machines.',
-                )}
-              </Text>
-            }
-            crumbs={['spec', 'controller_max_vm_inflight']}
-            onEdit={() => showModal(<EditMaxVMInFlightModal resource={obj} />)}
-          />
-
-          <DetailsItem
-            title={'Must gather cleanup after (hours)'}
-            content={mustGatherAPICleanupMaxAge || <span className="text-muted">{'Disabled'}</span>}
-            moreInfoLink={
-              'https://access.redhat.com/documentation/en-us/migration_toolkit_for_virtualization/2.4/html-single/installing_and_using_the_migration_toolkit_for_virtualization/index#advanced-migration-options'
-            }
-            helpContent={
-              <Text>
-                {t(
-                  "Specifies the duration for retaining 'must gather' reports before they are automatically deleted. The default value is -1, which implies automatic cleanup is disabled.",
-                )}
-              </Text>
-            }
-            crumbs={['spec', 'must_gather_api_cleanup_max_age']}
-            onEdit={() => showModal(<EditCleanupMaxAgeModal resource={obj} />)}
-          />
-
-          <DetailsItem
-            title={'Controller CPU limit'}
-            content={
-              obj?.spec?.['controller_container_limits_cpu'] || (
-                <span className="text-muted">{'500m'}</span>
-              )
-            }
-            moreInfoLink={
-              'https://access.redhat.com/documentation/en-us/migration_toolkit_for_virtualization/2.4/html-single/installing_and_using_the_migration_toolkit_for_virtualization/index#advanced-migration-options'
-            }
-            helpContent={
-              <Text>
-                {t(
-                  'Defines the CPU limits allocated to the controller container. The default value is 500 milliCPU.',
-                )}
-              </Text>
-            }
-            crumbs={['spec', 'controller_container_limits_cpu']}
-            onEdit={() => showModal(<EditControllerCPULimitModal resource={obj} />)}
-          />
-
-          <DetailsItem
-            title={'Controller Memory limit'}
-            content={
-              obj?.spec?.['controller_container_limits_memory'] || (
-                <span className="text-muted">{'800Mi'}</span>
-              )
-            }
-            moreInfoLink={
-              'https://access.redhat.com/documentation/en-us/migration_toolkit_for_virtualization/2.4/html-single/installing_and_using_the_migration_toolkit_for_virtualization/index#advanced-migration-options'
-            }
-            helpContent={
-              <Text>
-                {t(
-                  'Sets the memory limits allocated to the controller container. The default value is 800Mi.',
-                )}
-              </Text>
-            }
-            crumbs={['spec', 'controller_container_limits_memory']}
-            onEdit={() => showModal(<EditControllerMemoryLimitModal resource={obj} />)}
           />
         </DescriptionList>
       </CardBody>
