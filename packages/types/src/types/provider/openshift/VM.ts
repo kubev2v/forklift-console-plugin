b85380fc1ae3086af66776c579a4724f54c8db25
Radoslaw Szwajkowski <rszwajko@redhat.com>
2023-09-14 13:20:27 +0200

    Add V1VirtualMachine object to OpenshiftVM
    Add partial type definitions for KubeVirt VirtualMachine.
    
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- packages/types/src/types/provider/openshift/VM.ts
+++ packages/types/src/types/provider/openshift/VM.ts
@@ -1,7 +1,10 @@
+import { V1VirtualMachine } from '../../k8s/V1VirtualMachine';
 import { Concern } from '../base';
 
 import { OpenshiftResource } from './Resource';
 
+// https://github.com/kubev2v/forklift/blob/main/pkg/controller/provider/web/ocp/vm.go
 export interface OpenshiftVM extends OpenshiftResource {
   concerns: Concern[];
+  object: V1VirtualMachine;
 }
