965de1b9bc88330eaf8f7925d7a6c30e91dbde7e
Radoslaw Szwajkowski <rszwajko@redhat.com>
2023-11-08 20:23:04 +0100

    Filter VMs based on concerns label
    Create the list of available labels dynamically by traversing the list
    of already fetched VMs.
    
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

---
+++ packages/forklift-console-plugin/src/modules/Providers/views/details/tabs/VirtualMachines/utils/concernFilter.ts
@@ -0,0 +1,29 @@
+import { EnumToTuple, EnumValue } from '@kubev2v/common';
+
+export const concernFilter = (t: (string) => string) => ({
+  type: 'concerns',
+  primary: true,
+  placeholderLabel: t('Concerns'),
+  groups: [
+    { groupId: 'category', label: t('Category') },
+    { groupId: 'label', label: t('Concern') },
+  ],
+  dynamicFilter: (items: { vm: { concerns: { label: string }[] } }[]) => ({
+    values: [
+      ...EnumToTuple({
+        Critical: 'Critical',
+        Warning: 'Warning',
+        Information: 'Information',
+      }).map(({ id, label }: EnumValue): EnumValue => ({ id, label, groupId: 'category' })),
+      ...Array.from(
+        new Set(
+          items
+            .filter((item) => item?.vm?.concerns?.length)
+            .flatMap((item) => item.vm.concerns)
+            .map((concern) => concern.label),
+        ),
+      ) // at this point the list contains unique strings that can be used as ID
+        .map((label: string): EnumValue => ({ groupId: 'label', id: label, label })),
+    ],
+  }),
+});
