fc43fa2bf09d7857951b28be8e3d25b96d9c63c4
Yaacov Zamir <kobi.zamir@gmail.com>
2024-01-31 13:21:56 +0200

    Add migration hints and actions to provider view
    Signed-off-by: Yaacov Zamir <kobi.zamir@gmail.com>

--- packages/forklift-console-plugin/src/modules/Providers/views/details/tabs/Details/ProviderDetails.tsx
+++ packages/forklift-console-plugin/src/modules/Providers/views/details/tabs/Details/ProviderDetails.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import { Trans } from 'react-i18next';
+import { Link } from 'react-router-dom';
 import { RouteComponentProps } from 'react-router-dom';
 import { SectionHeading } from 'src/components/headers/SectionHeading';
 import { useGetDeleteAndEditAccessReview, useProviderInventory } from 'src/modules/Providers/hooks';
-import { ProviderData } from 'src/modules/Providers/utils';
+import { getResourceUrl, ProviderData } from 'src/modules/Providers/utils';
 import { useForkliftTranslation } from 'src/utils/i18n';
 
 import {
   ProviderInventory,
   ProviderModel,
   ProviderModelGroupVersionKind,
+  ProviderModelRef,
   V1beta1Provider,
 } from '@kubev2v/types';
 import { useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
-import { PageSection } from '@patternfly/react-core';
+import { Alert, PageSection } from '@patternfly/react-core';
+import BellIcon from '@patternfly/react-icons/dist/esm/icons/bell-icon';
 
 import {
   ConditionsSection,
@@ -37,9 +41,32 @@ export const ProviderDetails: React.FC<ProviderDetailsProps> = ({ obj, loaded, l
     return <></>;
   }
 
+  const providerURL = getResourceUrl({
+    reference: ProviderModelRef,
+    name: provider?.metadata?.name,
+    namespace: provider?.metadata?.namespace,
+  });
+
   return (
     <div>
-      <PageSection variant="light">
+      <PageSection variant="light" className="forklift-page-section--info">
+        <Alert
+          customIcon={<BellIcon />}
+          variant="warning"
+          title={t('How to create a migration plan')}
+        >
+          <Trans t={t} ns="plugin__forklift-console-plugin">
+            To migrate virtual machines from <strong>{provider.metadata.name}</strong> provider,
+            select the virtual machines to migrate from the list of available virtual machines
+            located in the virtual machines tab.{' '}
+            <Link to={`${providerURL}/vms`}>
+              Go to <strong>Virtual Machines</strong> tab
+            </Link>
+          </Trans>
+        </Alert>
+      </PageSection>
+
+      <PageSection variant="light" className="forklift-page-section--details">
         <SectionHeading text={t('Provider details')} />
         <DetailsSection data={obj} />
       </PageSection>
