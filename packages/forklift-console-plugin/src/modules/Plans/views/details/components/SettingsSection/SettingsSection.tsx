e35e657f74c56f300aa669fc05e99e97fcfd85a3
Sharon Gratch <sgratch@redhat.com>
2024-05-05 19:50:42 +0300

    Add the preserveStaticIPs flag to vSphere's plan details section
    Reference: https://github.com/kubev2v/forklift-console-plugin/issues/1133
    
    Add an option to set the preserveStaticIPs flag within the plans details section
    for the vSphere provider only.
    
    The default value is preserveStaticIPs=false
    
    Signed-off-by: Sharon Gratch <sgratch@redhat.com>

--- packages/forklift-console-plugin/src/modules/Plans/views/details/components/SettingsSection/SettingsSection.tsx
+++ packages/forklift-console-plugin/src/modules/Plans/views/details/components/SettingsSection/SettingsSection.tsx
@@ -10,6 +10,7 @@ import { Suspend } from '../Suspend';
 
 import {
   PreserveClusterCpuModelDetailsItem,
+  PreserveStaticIPsDetailsItem,
   TargetNamespaceDetailsItem,
   TransferNetworkDetailsItem,
   WarmDetailsItem,
@@ -80,6 +81,11 @@ export const SettingsSectionInternal: React.FC<SettingsSectionProps> = ({ obj, p
         {['ovirt'].includes(sourceProvider?.spec?.type) && (
           <Suspend obj={sourceProvider} loaded={loaded} loadError={loadError}>
             <PreserveClusterCpuModelDetailsItem resource={obj} canPatch={permissions.canPatch} />
+          </Suspend>
+        )}
+        {['vsphere'].includes(sourceProvider?.spec?.type) && (
+          <Suspend obj={sourceProvider} loaded={loaded} loadError={loadError}>
+            <PreserveStaticIPsDetailsItem resource={obj} canPatch={permissions.canPatch} />
           </Suspend>
         )}
       </DescriptionList>
--- packages/forklift-console-plugin/src/modules/Plans/views/details/components/SettingsSection/modals/index.ts
+++ packages/forklift-console-plugin/src/modules/Plans/views/details/components/SettingsSection/modals/index.ts
@@ -1,5 +1,6 @@
 // @index(['./*', /style/g], f => `export * from '${f.path}';`)
 export * from './EditPlanPreserveClusterCpuModel';
+export * from './EditPlanPreserveStaticIPs';
 export * from './EditPlanTargetNamespace';
 export * from './EditPlanTransferNetwork';
 export * from './EditPlanWarm';
