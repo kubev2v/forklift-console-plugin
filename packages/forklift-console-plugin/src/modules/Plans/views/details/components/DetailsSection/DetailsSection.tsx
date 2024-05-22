9e2064f1b8a572bd0a214e1ae11507dde99ca0be
yaacov <kobi.zamir@gmail.com>
2024-05-22 10:25:26 +0300

    Add a start plan button to plan page header
    Signed-off-by: yaacov <kobi.zamir@gmail.com>

--- packages/forklift-console-plugin/src/modules/Plans/views/details/components/DetailsSection/DetailsSection.tsx
+++ packages/forklift-console-plugin/src/modules/Plans/views/details/components/DetailsSection/DetailsSection.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
-import { PlanStartMigrationModal } from 'src/modules/Plans/modals';
-import {
-  canPlanReStart,
-  canPlanStart,
-  isPlanExecuting,
-  isPlanSucceeded,
-} from 'src/modules/Plans/utils';
-import { ModalHOC, useModal } from 'src/modules/Providers/modals';
-import { useForkliftTranslation } from 'src/utils/i18n';
 
-import { PlanModel, V1beta1Plan } from '@kubev2v/types';
-import { Button, DescriptionList, FlexItem, Level, LevelItem } from '@patternfly/react-core';
-import StartIcon from '@patternfly/react-icons/dist/esm/icons/play-icon';
-import ReStartIcon from '@patternfly/react-icons/dist/esm/icons/redo-icon';
+import { V1beta1Plan } from '@kubev2v/types';
+import { DescriptionList } from '@patternfly/react-core';
 
 import {
   CreatedAtDetailsItem,
@@ -22,78 +11,13 @@ import {
   StatusDetailsItem,
 } from './components';
 
-export const DetailsSection: React.FC<DetailsSectionProps> = (props) => (
-  <ModalHOC>
-    <DetailsSectionInternal {...props} />
-  </ModalHOC>
-);
-
 export type DetailsSectionProps = {
   obj: V1beta1Plan;
 };
 
-export const DetailsSectionInternal: React.FC<DetailsSectionProps> = ({ obj }) => {
-  const { t } = useForkliftTranslation();
-  const { showModal } = useModal();
-
-  const canStart = canPlanStart(obj);
-  const canReStart = canPlanReStart(obj);
-  const isExecuting = isPlanExecuting(obj);
-  const isSucceeded = isPlanSucceeded(obj);
-
-  const buttonStartLabel = canReStart ? t('Restart migration') : t('Start migration');
-  const buttonStartIcon = canReStart ? (
-    <ReStartIcon color="green" className="forklift-page-section--details-start-button__icon" />
-  ) : (
-    <StartIcon color="green" className="forklift-page-section--details-start-button__icon" />
-  );
-
-  const canNotRunIcon = (
-    <StartIcon color="gray" className="forklift-page-section--details-start-button__icon" />
-  );
-  let canNotRunLabel: string;
-  if (isExecuting) {
-    canNotRunLabel = t('Plan running');
-  } else if (isSucceeded) {
-    canNotRunLabel = t('Plan Succeeded');
-  } else {
-    canNotRunLabel = t('Plan not ready');
-  }
-
+export const DetailsSection: React.FC<DetailsSectionProps> = ({ obj }) => {
   return (
     <>
-      <FlexItem>
-        <div className="forklift-page-section--details-start-button">
-          <Button
-            isAriaDisabled={!canStart}
-            variant="link"
-            onClick={() =>
-              showModal(
-                <PlanStartMigrationModal
-                  resource={obj}
-                  model={PlanModel}
-                  title={buttonStartLabel}
-                />,
-              )
-            }
-          >
-            <Level hasGutter>
-              {canStart ? (
-                <>
-                  <LevelItem>{buttonStartIcon}</LevelItem>
-                  <LevelItem>{buttonStartLabel}</LevelItem>
-                </>
-              ) : (
-                <>
-                  <LevelItem>{canNotRunIcon}</LevelItem>
-                  <LevelItem>{canNotRunLabel}</LevelItem>
-                </>
-              )}
-            </Level>
-          </Button>
-        </div>
-      </FlexItem>
-
       <DescriptionList
         className="forklift-page-section--details-status"
         columnModifier={{
--- packages/forklift-console-plugin/src/modules/Plans/views/details/components/PlanPageHeadings.tsx
+++ packages/forklift-console-plugin/src/modules/Plans/views/details/components/PlanPageHeadings.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { PlanActionsDropdown } from 'src/modules/Plans/actions';
+import { PlanStartMigrationModal } from 'src/modules/Plans/modals';
+import { canPlanReStart, canPlanStart, getPlanPhase } from 'src/modules/Plans/utils';
 import { useGetDeleteAndEditAccessReview } from 'src/modules/Providers/hooks';
+import { useModal } from 'src/modules/Providers/modals';
 import { PageHeadings } from 'src/modules/Providers/utils';
+import { useForkliftTranslation } from 'src/utils';
 
 import { PlanModel, PlanModelGroupVersionKind, V1beta1Plan } from '@kubev2v/types';
 import { useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
-import { PageSection } from '@patternfly/react-core';
+import { Button, Level, LevelItem, PageSection } from '@patternfly/react-core';
+import StartIcon from '@patternfly/react-icons/dist/esm/icons/play-icon';
+import ReStartIcon from '@patternfly/react-icons/dist/esm/icons/redo-icon';
 
 import PlanCriticalCondition from './PlanCriticalCondition';
 
@@ -13,6 +19,9 @@ export const PlanPageHeadings: React.FC<{ name: string; namespace: string }> = (
   name,
   namespace,
 }) => {
+  const { t } = useForkliftTranslation();
+  const { showModal } = useModal();
+
   const [plan, loaded, error] = useK8sWatchResource<V1beta1Plan>({
     groupVersionKind: PlanModelGroupVersionKind,
     namespaced: true,
@@ -26,6 +35,13 @@ export const PlanPageHeadings: React.FC<{ name: string; namespace: string }> = (
   });
 
   const alerts = [];
+
+  const canStart = canPlanStart(plan);
+  const canReStart = canPlanReStart(plan);
+  const planStatus = getPlanPhase({ obj: plan });
+
+  const buttonStartLabel = canReStart ? t('Restart migration') : t('Start migration');
+  const buttonStartIcon = canReStart ? <ReStartIcon /> : <StartIcon />;
 
   const criticalCondition =
     loaded &&
@@ -42,13 +58,41 @@ export const PlanPageHeadings: React.FC<{ name: string; namespace: string }> = (
     );
   }
 
+  const actions = (
+    <Level hasGutter>
+      {canStart && (
+        <Button
+          variant="primary"
+          onClick={() =>
+            showModal(
+              <PlanStartMigrationModal
+                resource={plan}
+                model={PlanModel}
+                title={buttonStartLabel}
+              />,
+            )
+          }
+        >
+          <Level hasGutter>
+            <>
+              <LevelItem>{buttonStartIcon}</LevelItem>
+              <LevelItem>{buttonStartLabel}</LevelItem>
+            </>
+          </Level>
+        </Button>
+      )}
+      <PlanActionsDropdown data={{ obj: plan, permissions }} fieldId={''} fields={[]} />
+    </Level>
+  );
+
   return (
     <>
       <PageHeadings
         model={PlanModel}
         obj={plan}
         namespace={namespace}
-        actions={<PlanActionsDropdown data={{ obj: plan, permissions }} fieldId={''} fields={[]} />}
+        actions={actions}
+        status={planStatus}
       >
         {alerts && alerts.length > 0 && (
           <PageSection variant="light" className="forklift-page-headings-alerts">
