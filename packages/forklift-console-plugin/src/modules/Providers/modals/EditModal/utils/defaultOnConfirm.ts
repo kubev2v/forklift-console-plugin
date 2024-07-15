cd82e94d31937716bcf25a320fd0d9d2e88ff8e9
yaacov <kobi.zamir@gmail.com>
2024-07-15 14:14:35 +0300

    save ints where the controller is looking for numbers
    Signed-off-by: yaacov <kobi.zamir@gmail.com>

--- packages/forklift-console-plugin/src/modules/Providers/modals/EditModal/utils/defaultOnConfirm.ts
+++ packages/forklift-console-plugin/src/modules/Providers/modals/EditModal/utils/defaultOnConfirm.ts
@@ -1,11 +1,14 @@
 import { getValueByJsonPath, jsonPathToPatch } from 'src/modules/Providers/utils';
 
-import { k8sPatch } from '@openshift-console/dynamic-plugin-sdk';
+import { k8sPatch, K8sResourceCommon } from '@openshift-console/dynamic-plugin-sdk';
 
+/**
+ * Patches a Kubernetes resource with a new value.
+ */
 export const defaultOnConfirm = async ({ resource, jsonPath, model, newValue: value }) => {
   const op = getValueByJsonPath(resource, jsonPath) ? 'replace' : 'add';
 
-  await k8sPatch({
+  return await k8sPatch<K8sResourceCommon>({
     model: model,
     resource: resource,
     data: [
@@ -16,4 +19,15 @@ export const defaultOnConfirm = async ({ resource, jsonPath, model, newValue: va
       },
     ],
   });
+};
+
+/**
+ * Wraps the defaultOnConfirm method to convert the newValue from string to int before patching.
+ */
+export const defaultOnConfirmWithIntValue = async ({ resource, jsonPath, model, newValue }) => {
+  // Convert the newValue from string to int
+  const intValue = parseInt(newValue.toString(), 10);
+
+  // Call the original method with the converted value
+  return await defaultOnConfirm({ resource, jsonPath, model, newValue: intValue });
 };
