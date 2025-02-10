810352f12a67352cba3d7ecdba6655f57918336e
Jeff Puzzo <jpuzzo@redhat.com>
2025-02-10 09:44:15 -0500

    [MTV-1871] Sorting virtual machines isn't saved while refresh
    Signed-off-by: Jeff Puzzo <jpuzzo@redhat.com>

---
+++ packages/forklift-console-plugin/src/components/TableSortContext.tsx
@@ -0,0 +1,36 @@
+import React, { createContext, FC, PropsWithChildren, useContext } from 'react';
+
+import { ResourceField, SortType, useSort } from '@kubev2v/common';
+
+type TableSortContextProps = {
+  activeSort: SortType;
+  setActiveSort: (activeSort: SortType) => void;
+  compareFn: (a: unknown, b: unknown) => number;
+};
+
+const defaultTableSortContext = {
+  activeSort: { isAsc: true, resourceFieldId: undefined, label: '' },
+  setActiveSort: () => undefined,
+  compareFn: () => undefined,
+};
+
+const TableSortContext = createContext<TableSortContextProps>(defaultTableSortContext);
+
+type TableSortContextProviderProps = PropsWithChildren & {
+  fields: ResourceField[];
+};
+
+export const TableSortContextProvider: FC<TableSortContextProviderProps> = ({
+  fields,
+  children,
+}) => {
+  const [activeSort, setActiveSort, compareFn] = useSort(fields);
+
+  return (
+    <TableSortContext.Provider value={{ activeSort, setActiveSort, compareFn }}>
+      {children}
+    </TableSortContext.Provider>
+  );
+};
+
+export const useTableSortContext = () => useContext(TableSortContext);
