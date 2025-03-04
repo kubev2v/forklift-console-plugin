a216c26bc762c446cc7f0515b36312ddb0e44efe
Matan Schatzman <mschatzm@redhat.com>
2025-03-04 11:45:29 +0200

    Add env to container
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- testing/cypress.config.cjs
+++ testing/cypress.config.cjs
@@ -4,7 +4,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.BRIDGE_BASE_ADDRESS||process.env.BASE_ADDRESS || "http://host.docker.internal:9000",
+    baseUrl: process.env.BRIDGE_BASE_ADDRESS||process.env.BASE_ADDRESS,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
@@ -14,8 +14,6 @@ export default defineConfig({
   trashAssetsBeforeRuns: true,
   video: true,
   videosFolder: '/tmp/gui-test-screenshots/videos/',
-  viewportHeight: 1440,
-  viewportWidth: 2560,
   waitForAnimations: true,
   watchForFileChanges: false,
   viewportWidth: 1920,
