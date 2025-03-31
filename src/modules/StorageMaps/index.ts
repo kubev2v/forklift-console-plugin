4eea9f65827ce2da85b4847c4d2ec4b9909920f5
Joachim Schuler <jschuler@redhat.com>
2025-03-31 17:03:12 -0400

    Re-run
    Signed-off-by: Joachim Schuler <jschuler@redhat.com>

--- src/modules/StorageMaps/index.ts
+++
@@ -1,7 +0,0 @@
-// @index(['./*', /__/g], f => `export * from '${f.path}';`)
-export * from './actions';
-export * from './components';
-export * from './utils';
-export * from './views';
-export * from './yamlTemplates';
-// @endindex
