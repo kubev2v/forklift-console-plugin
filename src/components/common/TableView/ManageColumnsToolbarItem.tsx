c5c3f4ceca8585cf331a20c4bf60cad3156fb570
Aviv Turgeman <aturgema@redhat.com>
2025-05-29 11:22:16 +0300

    MTV-2624: Add Manage columns text to the manage columns button on MTV list pages
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/components/common/TableView/ManageColumnsToolbarItem.tsx
+++ src/components/common/TableView/ManageColumnsToolbarItem.tsx
@@ -1,7 +1,15 @@
 import type { ReactNode } from 'react';
 
-import { Button, ToolbarItem, Tooltip } from '@patternfly/react-core';
+import {
+  Button,
+  ButtonVariant,
+  Split,
+  SplitItem,
+  ToolbarItem,
+  Tooltip,
+} from '@patternfly/react-core';
 import { ColumnsIcon } from '@patternfly/react-icons';
+import { useForkliftTranslation } from '@utils/i18n';
 
 type ManageColumnsToolbarItemProps = {
   children: ReactNode;
@@ -19,23 +27,28 @@ type ManageColumnsToolbarItemProps = {
   ariaLabel?: string;
 };
 
-/**
- * A Toolbar button item with columns icon, uses to show the Manage Columns dialog modal.
- *
- * [<img src="static/media/src/components-stories/assets/github-logo.svg"><i class="fi fi-brands-github">
- * <font color="green">View component source on GitHub</font>](https://github.com/kubev2v/forklift-console-plugin/blob/main/packages/common/src/components/TableView/ManageColumnsToolbarItem.tsx)
- */
 export const ManageColumnsToolbarItem = ({
-  ariaLabel = 'Manage Columns',
+  ariaLabel,
   children,
   showDialog,
-  tooltip = 'Manage Columns',
+  tooltip,
 }: ManageColumnsToolbarItemProps) => {
+  const { t } = useForkliftTranslation();
+  const manageColumnsText = t('Manage columns');
   return (
     <ToolbarItem>
-      <Tooltip content={tooltip}>
-        <Button variant="plain" onClick={showDialog} aria-label={ariaLabel}>
-          <ColumnsIcon />
+      <Tooltip content={tooltip ?? manageColumnsText}>
+        <Button
+          variant={ButtonVariant.plain}
+          onClick={showDialog}
+          aria-label={ariaLabel ?? manageColumnsText}
+        >
+          <Split hasGutter>
+            <SplitItem>
+              <ColumnsIcon />
+            </SplitItem>
+            <SplitItem>{manageColumnsText}</SplitItem>
+          </Split>
         </Button>
       </Tooltip>
       {children}
