3d1954be887317e8c19a6ed8ff3bca04c935741a
Aviv Turgeman <aturgema@redhat.com>
2025-07-01 12:10:44 +0300

    MTV-2721: Update the message with "shared" field for PVC name template
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/plans/details/tabs/Details/components/SettingsSection/components/PVCNameTemplate/utils/constants.ts
+++ src/plans/details/tabs/Details/components/SettingsSection/components/PVCNameTemplate/utils/constants.ts
@@ -5,6 +5,7 @@ export const pvcNameTemplateAllowedVariables = [
   t('- .PlanName: name of the migration plan'),
   t('- .DiskIndex: initial volume index of the disk'),
   t('- .RootDiskIndex: index of the root disk'),
+  t('- .Shared: true if the volume is shared by multiple VMs, false otherwise'),
 ];
 export const pvcNameTemplateHelperExamples = [
   '"{{.VmName}}-disk-{{.DiskIndex}}"',
