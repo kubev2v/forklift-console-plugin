58e835d61f0663fcc86e46d0c14f8aa0a2e21741
Aviv Turgeman <aturgema@redhat.com>
2025-07-03 11:16:16 +0300

    MTV-2974: [UI] Plan settings page refresh too fast, can't do settings on the page (#1689)
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/plans/details/hooks/usePlanPages.tsx
+++ src/plans/details/hooks/usePlanPages.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+
 import type { NavPage } from '@openshift-console/dynamic-plugin-sdk';
 import { useForkliftTranslation } from '@utils/i18n';
 
@@ -10,38 +12,42 @@ import PlanYAMLPage from '../tabs/YAML/PlanYAMLPage';
 
 const usePlanPages = (name: string, namespace: string) => {
   const { t } = useForkliftTranslation();
-  const pages: NavPage[] = [
-    {
-      component: () => <PlanDetailsPage name={name} namespace={namespace} />,
-      href: '',
-      name: t('Details'),
-    },
-    {
-      component: () => <PlanYAMLPage name={name} namespace={namespace} />,
-      href: 'yaml',
-      name: t('YAML'),
-    },
-    {
-      component: () => <PlanVirtualMachinesPage name={name} namespace={namespace} />,
-      href: 'vms',
-      name: t('Virtual machines'),
-    },
-    {
-      component: () => <PlanResourcesPage name={name} namespace={namespace} />,
-      href: 'resources',
-      name: t('Resources'),
-    },
-    {
-      component: () => <PlanMappingsPage name={name} namespace={namespace} />,
-      href: 'mappings',
-      name: t('Mappings'),
-    },
-    {
-      component: () => <PlanHooksPage name={name} namespace={namespace} />,
-      href: 'hooks',
-      name: t('Hooks'),
-    },
-  ];
+
+  const pages: NavPage[] = useMemo(
+    () => [
+      {
+        component: () => <PlanDetailsPage name={name} namespace={namespace} />,
+        href: '',
+        name: t('Details'),
+      },
+      {
+        component: () => <PlanYAMLPage name={name} namespace={namespace} />,
+        href: 'yaml',
+        name: t('YAML'),
+      },
+      {
+        component: () => <PlanVirtualMachinesPage name={name} namespace={namespace} />,
+        href: 'vms',
+        name: t('Virtual machines'),
+      },
+      {
+        component: () => <PlanResourcesPage name={name} namespace={namespace} />,
+        href: 'resources',
+        name: t('Resources'),
+      },
+      {
+        component: () => <PlanMappingsPage name={name} namespace={namespace} />,
+        href: 'mappings',
+        name: t('Mappings'),
+      },
+      {
+        component: () => <PlanHooksPage name={name} namespace={namespace} />,
+        href: 'hooks',
+        name: t('Hooks'),
+      },
+    ],
+    [name, namespace, t],
+  );
 
   return pages;
 };
