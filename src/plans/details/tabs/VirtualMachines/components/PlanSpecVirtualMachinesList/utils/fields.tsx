0c0f18a5ce7d2fec70ea7adea89a15713d445d26
Aviv Turgeman <aturgema@redhat.com>
2025-07-17 10:39:06 +0300

    MTV-2942: show VM concerns in VM tab plan details
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/plans/details/tabs/VirtualMachines/components/PlanSpecVirtualMachinesList/utils/fields.tsx
+++ src/plans/details/tabs/VirtualMachines/components/PlanSpecVirtualMachinesList/utils/fields.tsx
@@ -1,16 +1,19 @@
-import PlanConditionsField from '../components/PlanConditionsField';
+import { VMConcernsCellRenderer } from 'src/modules/Providers/views/details/tabs/VirtualMachines/components/VMConcernsCellRenderer';
+
 import SpecVirtualMachinesActions from '../components/SpecVirtualMachinesActions';
 
 import { PlanSpecVirtualMachinesTableResourceId, type SpecVirtualMachinePageData } from './types';
 
 export const getSpecVirtualMachinesRowFields = (fieldsData: SpecVirtualMachinePageData) => {
-  const { conditions, plan, vmIndex } = fieldsData;
+  const { inventoryVmData, plan, vmIndex } = fieldsData;
+
   return {
-    [PlanSpecVirtualMachinesTableResourceId.Actions]: (
-      <SpecVirtualMachinesActions plan={plan} vmIndex={vmIndex} />
-    ),
-    [PlanSpecVirtualMachinesTableResourceId.Conditions]: (
-      <PlanConditionsField conditions={conditions} />
+    [PlanSpecVirtualMachinesTableResourceId.Actions]:
+      inventoryVmData.vm?.providerType === 'vsphere' ? (
+        <SpecVirtualMachinesActions plan={plan} vmIndex={vmIndex} />
+      ) : null,
+    [PlanSpecVirtualMachinesTableResourceId.Concerns]: (
+      <VMConcernsCellRenderer data={inventoryVmData} fieldId="" fields={[]} />
     ),
     [PlanSpecVirtualMachinesTableResourceId.Name]: <>{fieldsData?.specVM?.name}</>,
   };
