1a3229dd83993b5650dd2de0f64a7aea98c50f9c
Aviv Turgeman <aturgema@redhat.com>
2025-07-17 10:39:06 +0300

    MTV-2942: show VM concerns in VM tab plan details
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/plans/details/tabs/VirtualMachines/components/PlanSpecVirtualMachinesList/hooks/useSpecVirtualMachinesListData.ts
+++ src/plans/details/tabs/VirtualMachines/components/PlanSpecVirtualMachinesList/hooks/useSpecVirtualMachinesListData.ts
@@ -1,18 +1,35 @@
-import type { V1beta1Plan } from '@kubev2v/types';
+import type { VmData } from 'src/modules/Providers/views/details/tabs/VirtualMachines/components/VMCellProps';
+import { useInventoryVms } from 'src/modules/Providers/views/details/tabs/VirtualMachines/utils/hooks/useInventoryVms';
+import usePlanSourceProvider from 'src/plans/details/hooks/usePlanSourceProvider';
+
+import type { V1beta1Plan, V1beta1Provider } from '@kubev2v/types';
 import { getPlanVirtualMachines } from '@utils/crds/plans/selectors';
 
 import { getPlanVirtualMachinesDict } from '../../utils/utils';
 import type { SpecVirtualMachinePageData } from '../utils/types';
 import { getPlanConditionsDict } from '../utils/utils';
 
-export const useSpecVirtualMachinesListData = (plan: V1beta1Plan) => {
+export const useSpecVirtualMachinesListData = (
+  plan: V1beta1Plan,
+): [SpecVirtualMachinePageData[], V1beta1Provider] => {
   const virtualMachines = getPlanVirtualMachines(plan);
+
+  const { sourceProvider } = usePlanSourceProvider(plan);
+  const [vmInventoryData, loading] = useInventoryVms({ provider: sourceProvider });
+  if (loading) {
+    return [[], sourceProvider];
+  }
+  const inventoryVmDict = vmInventoryData.reduce<Record<string, VmData>>((acc, vmData) => {
+    acc[vmData.vm.id] = vmData;
+    return acc;
+  }, {});
 
   const vmDict = getPlanVirtualMachinesDict(plan);
   const conditionsDict = getPlanConditionsDict(plan);
 
   const specVirtualMachinesListData = virtualMachines.map((specVM, vmIndex) => ({
     conditions: conditionsDict[specVM.id!],
+    inventoryVmData: inventoryVmDict[specVM.id!],
     plan,
     specVM,
     statusVM: vmDict[specVM.id!],
@@ -20,5 +37,5 @@ export const useSpecVirtualMachinesListData = (plan: V1beta1Plan) => {
     vmIndex,
   })) as SpecVirtualMachinePageData[];
 
-  return specVirtualMachinesListData;
+  return [specVirtualMachinesListData, sourceProvider];
 };
