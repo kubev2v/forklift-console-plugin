123fb81143f3ff048d40ce5a98dbc6480bc9048b
Pedro-S-Abreu <33811116+Pedro-S-Abreu@users.noreply.github.com>
2025-08-05 07:18:25 -0400

    MTV-3101: Remediate UI tests flakiness (#1756)
    * Revert to package-lock.json lockfileVersion@1 after #766 (#772)
    
    Co-authored-by: Gilles Dubreuil <gilles@redhat.com>
    Signed-off-by: Pedro Abreu <pabreu@redhat.com>
    
    * Playwright: automatically retry on GH action
    
    Signed-off-by: Pedro Abreu <pabreu@redhat.com>
    
    * force pw fail
    
    Signed-off-by: Pedro Abreu <pabreu@redhat.com>
    
    * revert foce pw fail
    
    Signed-off-by: Pedro Abreu <pabreu@redhat.com>
    
    ---------
    
    Signed-off-by: Pedro Abreu <pabreu@redhat.com>
    Co-authored-by: Mike Turley <mike.turley@alum.cs.umass.edu>
    Co-authored-by: Gilles Dubreuil <gilles@redhat.com>
    Co-authored-by: Pedro Abreu <pabreu@redhat.com>

--- testing/playwright.config.ts
+++ testing/playwright.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './playwright/e2e',
   timeout: 60_000,
+
+  retries: process.env.GITHUB_ACTIONS ? 3 : 0,
+
   use: {
     // GitHub Actions uses port 30080, local dev uses 9000
     baseURL: process.env.BRIDGE_BASE_ADDRESS ?? process.env.BASE_ADDRESS ?? 'http://localhost:9000',
