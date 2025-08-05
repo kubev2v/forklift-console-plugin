d4bc60ce31a81337219c503116f6a5bb6ac4e4e6
Pedro Abreu <pabreu@redhat.com>
2025-08-05 07:00:43 -0400

    revert foce pw fail
    Signed-off-by: Pedro Abreu <pabreu@redhat.com>

--- testing/playwright/e2e/end-to-end-migration.spec.ts
+++ testing/playwright/e2e/end-to-end-migration.spec.ts
@@ -70,7 +70,7 @@ test.describe('Plans - Critical End-to-End Migration', () => {
     await createWizard.waitForPlanCreation();
     await planDetailsPage.waitForPageLoad();
     await planDetailsPage.verifyBasicPlanDetailsPage({
-      planName: `${TEST_DATA.planName}a`,
+      planName: TEST_DATA.planName,
       sourceProvider: TEST_DATA.sourceProvider,
       targetProvider: TEST_DATA.targetProvider,
       targetProject: TEST_DATA.targetProject,
