b9c5aa93c621a10c692ff78ed77cafa86f89244f
Pedro Abreu <pabreu@redhat.com>
2025-10-02 17:41:34 -0400

    upgrade playwright to 1.55
    Resolves: MTV-3470
    Signed-off-by: Pedro Abreu <pabreu@redhat.com>

--- testing/playwright.config.ts
+++ testing/playwright.config.ts
@@ -6,10 +6,22 @@ export default defineConfig({
   globalSetup: './playwright/global.setup.ts',
   globalTeardown: './playwright/global.teardown.ts',
   testDir: './playwright/e2e',
-  timeout: process.env.JENKINS ? 120_000 : 60_000,
+  timeout: process.env.JENKINS ? 15 * 60_000 : 60_000,
   fullyParallel: true,
+  workers: 3,
 
   retries: process.env.GITHUB_ACTIONS ? 3 : 0,
+
+  reporter: [['list'], ['html', { open: 'never' }]],
+
+  use: {
+    actionTimeout: 20_000,
+    navigationTimeout: 20_000,
+  },
+
+  expect: {
+    timeout: 20_000,
+  },
 
   projects: [
     {
@@ -24,6 +36,7 @@ export default defineConfig({
         viewport: { width: 1920, height: 1080 },
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
+        trace: 'retain-on-failure',
         // Use data-testid to match actual rendered HTML
         testIdAttribute: 'data-testid',
         ignoreHTTPSErrors: true,
