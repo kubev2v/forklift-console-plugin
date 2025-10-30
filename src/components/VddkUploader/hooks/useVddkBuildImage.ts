260209ec598036dd1aebe2e8a970e03de799f9bc
Aviv Turgeman <aturgema@redhat.com>
2025-10-30 10:08:45 +0200

    Resolves: MTV-3618 | VDDK init image archive upload fails when creating provider from All projects (#2066)
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/components/VddkUploader/hooks/useVddkBuildImage.ts
+++ src/components/VddkUploader/hooks/useVddkBuildImage.ts
@@ -1,11 +1,16 @@
 import { useActiveNamespace, useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
+import { Namespace } from '@utils/constants';
 import { isEmpty } from '@utils/helpers';
+import { getDefaultNamespace } from '@utils/namespaces';
 
 import type { VddkBuild } from '../utils/types';
 import { getVddkImageBuildResponse } from '../utils/utils';
 
 export const useVddkBuildImage = (buildName: string) => {
   const [activeNamespace] = useActiveNamespace();
+  const namespace =
+    activeNamespace === Namespace.AllProjects ? getDefaultNamespace() : activeNamespace;
+
   const [vddkBuild] = useK8sWatchResource<VddkBuild>(
     buildName
       ? {
@@ -15,7 +20,7 @@ export const useVddkBuildImage = (buildName: string) => {
             version: 'v1',
           },
           name: buildName,
-          namespace: activeNamespace,
+          namespace,
         }
       : null,
   );
