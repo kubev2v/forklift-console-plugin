c6b855dcc8ea7433185011883ce226ec302f3bb7
jpuzz0 <96431149+jpuzz0@users.noreply.github.com>
2026-01-15 10:28:43 -0500

    Resolves: MTV-4309 | [UI] Extra YAML subtitle element present on Overview page (#2190)
    Signed-off-by: Jeff Puzzo <jpuzzo@redhat.com>

--- src/overview/tabs/YAML/ForkliftControllerYAMLTab.tsx
+++ src/overview/tabs/YAML/ForkliftControllerYAMLTab.tsx
@@ -1,12 +1,11 @@
 import { type FC, Suspense } from 'react';
 import Loading from 'src/overview/components/Loading';
-import TabTitle from 'src/overview/components/TabTitle';
 import { useK8sWatchForkliftController } from 'src/overview/hooks/useK8sWatchForkliftController';
 import { useForkliftTranslation } from 'src/utils/i18n';
 
 import { ResourceYAMLEditorWrapper } from '@components/ResourceYAMLEditorWrapper/ResourceYAMLEditorWrapper';
 import { ResourceYAMLEditor } from '@openshift-console/dynamic-plugin-sdk';
-import { Bullseye, CardTitle } from '@patternfly/react-core';
+import { Bullseye } from '@patternfly/react-core';
 
 const ForkliftControllerYAMLTab: FC = () => {
   const [forkliftController, loaded, loadError] = useK8sWatchForkliftController();
@@ -19,15 +18,6 @@ const ForkliftControllerYAMLTab: FC = () => {
         </Bullseye>
       }
     >
-      <CardTitle className="forklift-title pf-v6-u-p-lg pf-v6-u-pb-0">
-        <TabTitle
-          title={t('YAML')}
-          helpContent={t(
-            'You can write configuration files for the migration toolkit for virtualization using YAML, a human-readable data serialization language.',
-          )}
-        />
-      </CardTitle>
-
       {forkliftController && loaded && !loadError && (
         <ResourceYAMLEditorWrapper>
           <ResourceYAMLEditor
