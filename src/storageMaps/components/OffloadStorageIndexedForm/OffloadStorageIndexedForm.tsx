b6d9ff2f435916c72f233e50dcf71b0aca31d7b6
Aviv Turgeman <aturgema@redhat.com>
2026-02-26 14:58:23 +0200

    Resolves: MTV-4583, MTV-4584, MTV-4585
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/storageMaps/components/OffloadStorageIndexedForm/OffloadStorageIndexedForm.tsx
+++ src/storageMaps/components/OffloadStorageIndexedForm/OffloadStorageIndexedForm.tsx
@@ -1,11 +1,25 @@
-import { type FC, useState } from 'react';
+import { type FC, useCallback, useMemo, useState } from 'react';
+import { useFormContext, useWatch } from 'react-hook-form';
 import { getStorageMapFieldId } from 'src/storageMaps/utils/getStorageMapFieldId';
+import { validateOffloadFields } from 'src/storageMaps/utils/validateOffloadFields';
 
 import type { V1beta1Provider } from '@forklift-ui/types';
-import { ExpandableSection, Form, Split, SplitItem } from '@patternfly/react-core';
+import {
+  Button,
+  ButtonVariant,
+  ExpandableSection,
+  Form,
+  FormGroup,
+  HelperText,
+  HelperTextItem,
+  Split,
+  SplitItem,
+  Tooltip,
+} from '@patternfly/react-core';
+import { MinusCircleIcon } from '@patternfly/react-icons';
 import { useForkliftTranslation } from '@utils/i18n';
 
-import { StorageMapFieldId } from '../../utils/types';
+import { StorageMapFieldId, type StorageMapping } from '../../utils/types';
 
 import OffloadPluginField from './OffloadPluginField';
 import StorageProductField from './StorageProductField';
@@ -24,6 +38,37 @@ const OffloadStorageIndexedForm: FC<OffloadStorageIndexedFormProps> = ({
 }) => {
   const { t } = useForkliftTranslation();
   const [isExpanded, setIsExpanded] = useState(false);
+  const { control, setValue } = useFormContext();
+
+  const pluginFieldId = getStorageMapFieldId(StorageMapFieldId.OffloadPlugin, index);
+  const secretFieldId = getStorageMapFieldId(StorageMapFieldId.StorageSecret, index);
+  const productFieldId = getStorageMapFieldId(StorageMapFieldId.StorageProduct, index);
+
+  const [offloadPlugin, storageSecret, storageProduct] = useWatch({
+    control,
+    name: [pluginFieldId, secretFieldId, productFieldId],
+  });
+
+  const hasAnyOffloadValue =
+    Boolean(offloadPlugin) || Boolean(storageSecret) || Boolean(storageProduct);
+
+  const offloadError = useMemo((): string | undefined => {
+    const mapping = {
+      [StorageMapFieldId.OffloadPlugin]: offloadPlugin,
+      [StorageMapFieldId.SourceStorage]: { name: '' },
+      [StorageMapFieldId.StorageProduct]: storageProduct,
+      [StorageMapFieldId.StorageSecret]: storageSecret,
+      [StorageMapFieldId.TargetStorage]: { name: '' },
+    } satisfies StorageMapping;
+
+    return validateOffloadFields(mapping);
+  }, [offloadPlugin, storageProduct, storageSecret]);
+
+  const clearOffloadFields = useCallback((): void => {
+    setValue(pluginFieldId, '', { shouldDirty: true, shouldValidate: true });
+    setValue(secretFieldId, '', { shouldDirty: true, shouldValidate: true });
+    setValue(productFieldId, '', { shouldDirty: true, shouldValidate: true });
+  }, [pluginFieldId, productFieldId, secretFieldId, setValue]);
 
   return (
     <ExpandableSection
@@ -39,16 +84,31 @@ const OffloadStorageIndexedForm: FC<OffloadStorageIndexedFormProps> = ({
       isIndented
     >
       <Form className="offload-storage__form">
-        <OffloadPluginField
-          fieldId={getStorageMapFieldId(StorageMapFieldId.OffloadPlugin, index)}
-        />
-        <StorageSecretField
-          fieldId={getStorageMapFieldId(StorageMapFieldId.StorageSecret, index)}
-          sourceProvider={sourceProvider}
-        />
-        <StorageProductField
-          fieldId={getStorageMapFieldId(StorageMapFieldId.StorageProduct, index)}
-        />
+        <OffloadPluginField fieldId={pluginFieldId} />
+        <StorageSecretField fieldId={secretFieldId} sourceProvider={sourceProvider} />
+        <StorageProductField fieldId={productFieldId} />
+        {offloadError && (
+          <HelperText>
+            <HelperTextItem variant="error">{offloadError}</HelperTextItem>
+          </HelperText>
+        )}
+        <FormGroup className="fit-content">
+          <Tooltip content={t('Clear offload options')}>
+            <Button
+              icon={<MinusCircleIcon />}
+              isInline
+              variant={ButtonVariant.plain}
+              aria-label={t('Clear offload options')}
+              isDisabled={!hasAnyOffloadValue}
+              onClick={(e) => {
+                e.stopPropagation();
+                clearOffloadFields();
+              }}
+            >
+              {t('Clear')}
+            </Button>
+          </Tooltip>
+        </FormGroup>
       </Form>
     </ExpandableSection>
   );
