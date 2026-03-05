9e32dae3fd0ec730c317c9b6a939149e93cda76f
Aviv Turgeman <aturgema@redhat.com>
2026-03-05 16:58:59 +0200

    Resolves: MTV-3918 | Support es/fr/ja/ko/zh-cn languages
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

---
+++ i18n-scripts/memsource-upload.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+source ./i18n-scripts/languages.sh
+
+while getopts v:s: flag
+do
+  case "${flag}" in
+    v) VERSION=${OPTARG};;
+    s) SPRINT=${OPTARG};;
+    *) echo "usage: $0 [-v] [-s]" >&2
+       exit 1;;
+  esac
+done
+
+BRANCH=$(git branch --show-current)
+
+echo "Creating project with title \"[Forklift $VERSION] UI Localization - Sprint $SPRINT/Branch $BRANCH\""
+
+echo "Exporting PO files"
+npm run export-pos
+echo "Exported all PO files"
+
+PROJECT_INFO=$(memsource project create --name "[Forklift $VERSION] UI Localization - Sprint $SPRINT/Branch $BRANCH" --template-id 169304 -f json)
+PROJECT_ID=$(echo "$PROJECT_INFO" | jq -r '.uid')
+
+echo "Creating jobs for exported PO files"
+for i in "${LANGUAGES[@]}"
+do
+  memsource job create --filenames po-files/"$i"/*.po --target-langs "$i" --project-id "${PROJECT_ID}"
+done
+
+echo "Uploaded PO files to Memsource"
+
+rm -rf po-files
---
+++ i18n-scripts/set-english-defaults.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const path = require('path');
+const pluralize = require('pluralize');
+const common = require('./common.js');
+
+const publicDir = path.join(__dirname, './../locales/');
+
+/**
+ * {{count}} item_other --> 0
+ * {{count}} item_one --> 1
+ * item_other --> 2
+ * item_one --> 3
+ */
+function determineRule(key) {
+  const withCount = key.startsWith('{{count}}');
+
+  if (withCount && key.includes('_other')) {
+    return 0;
+  }
+  if (withCount) {
+    return 1;
+  }
+  if (key.includes('_other')) {
+    return 2;
+  }
+  if (key.includes('_one')) {
+    return 3;
+  }
+  return 4;
+}
+
+function updateFile(fileName) {
+  const file = require(fileName);
+  const updatedFile = {};
+
+  const keys = Object.keys(file);
+
+  let originalKey;
+  let wordWithNoCount;
+
+  for (let i = 0; i < keys.length; i++) {
+    switch (determineRule(keys[i])) {
+      case 0:
+        [originalKey] = keys[i].split('_other');
+        wordWithNoCount = originalKey.replace('{{count}}', '').trim();
+        updatedFile[keys[i]] = `{{count}} ${pluralize(wordWithNoCount)}`;
+        break;
+      case 1:
+        [originalKey] = keys[i].split('_one');
+        wordWithNoCount = originalKey.replace('{{count}}', '').trim();
+        updatedFile[keys[i]] = `{{count}} ${pluralize.singular(wordWithNoCount)}`;
+        break;
+      case 2:
+        [originalKey] = keys[i].split('_other');
+        updatedFile[keys[i]] = originalKey;
+        break;
+      case 3:
+        [originalKey] = keys[i].split('_one');
+        updatedFile[keys[i]] = originalKey;
+        break;
+      default:
+        updatedFile[keys[i]] = keys[i];
+    }
+  }
+
+  fs.promises
+    .writeFile(fileName, JSON.stringify(updatedFile, null, 2))
+    .catch((e) => console.error(fileName, e));
+}
+
+function processLocalesFolder(filePath) {
+  if (path.basename(filePath) === 'en') {
+    common.parseFolder(filePath, updateFile);
+  }
+}
+
+common.parseFolder(publicDir, processLocalesFolder);
