47f485c213a1c8fc1ef89994fd44332af8de05cb
Pedro Abreu <pabreu@redhat.com>
2026-03-27 16:18:40 -0400

    Extract shared jest.mock setup into setup-provider-form-mocks
    Move the 60 lines of identical mock boilerplate from each test
    file into a shared setup module, reducing code duplication from
    80% to near zero across the 5 provider form test files.
    
    Resolves: None
    Signed-off-by: Pedro Abreu <pabreu@redhat.com>

--- src/providers/create/__tests__/CreateProviderForm.vsphere.test.tsx
+++ src/providers/create/__tests__/CreateProviderForm.vsphere.test.tsx
@@ -2,77 +2,11 @@ import { beforeEach, describe, expect, it } from '@jest/globals';
 import { render, screen, waitFor } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 
+import './setup-provider-form-mocks';
+
 import CreateProviderForm from '../CreateProviderForm';
 
-import {
-  clearAllProviderMocks,
-  mockCreateProvider,
-  mockCreateProviderSecret,
-  mockNavigate,
-  mockPatchProviderSecretOwner,
-  mockSearchParams,
-} from './test-utils';
-
-jest.mock('react-router-dom-v5-compat', () => ({
-  ...jest.requireActual('react-router-dom-v5-compat'),
-  useNavigate: () => mockNavigate,
-  useSearchParams: () => [mockSearchParams, jest.fn()],
-}));
-
-jest.mock('@utils/analytics/hooks/useForkliftAnalytics', () => ({
-  useForkliftAnalytics: () => ({
-    trackEvent: jest.fn(),
-  }),
-}));
-
-jest.mock('src/providers/create/utils/createProvider', () => ({
-  createProvider: (...args: unknown[]) => mockCreateProvider(...args),
-}));
-
-jest.mock('src/providers/create/utils/createProviderSecret', () => ({
-  createProviderSecret: (...args: unknown[]) => mockCreateProviderSecret(...args),
-}));
-
-jest.mock('src/providers/create/utils/patchProviderSecretOwner', () => ({
-  patchProviderSecretOwner: (...args: unknown[]) => mockPatchProviderSecretOwner(...args),
-}));
-
-jest.mock('@utils/hooks/useWatchProjectNames', () => ({
-  __esModule: true,
-  default: () => [['test-namespace'], true],
-}));
-
-jest.mock('../CreateProviderFormContextProvider', () => {
-  const { CreateProviderFormContext } = jest.requireActual('../constants');
-  return {
-    __esModule: true,
-    default: ({ children }: { children: React.ReactNode }) => (
-      <CreateProviderFormContext.Provider
-        value={{
-          providerNames: ['existing-provider'],
-          providerNamesLoaded: true,
-        }}
-      >
-        {children}
-      </CreateProviderFormContext.Provider>
-    ),
-  };
-});
-
-jest.mock('@openshift-console/dynamic-plugin-sdk', () => ({
-  useActiveNamespace: () => ['test-namespace', jest.fn()],
-  useK8sWatchResource: () => [[], true, null],
-  useModal: () => ({ isOpen: false, showModal: jest.fn(), hideModal: jest.fn() }),
-  useAccessReview: () => [true, false],
-  ProjectModel: {
-    apiGroup: 'project.openshift.io',
-    plural: 'projects',
-  },
-}));
-
-jest.mock('@utils/hooks/useDefaultProject', () => ({
-  useDefaultProject: () => 'test-namespace',
-}));
+import { clearAllProviderMocks } from './test-utils';
 
 jest.mock('../fields/ProviderTypeField', () =>
   // eslint-disable-next-line @typescript-eslint/no-require-imports
---
+++ src/providers/create/__tests__/setup-provider-form-mocks.tsx
@@ -0,0 +1,68 @@
+import {
+  mockCreateProvider,
+  mockCreateProviderSecret,
+  mockNavigate,
+  mockPatchProviderSecretOwner,
+  mockSearchParams,
+} from './test-utils';
+
+jest.mock('react-router-dom-v5-compat', () => ({
+  ...jest.requireActual('react-router-dom-v5-compat'),
+  useNavigate: () => mockNavigate,
+  useSearchParams: () => [mockSearchParams, jest.fn()],
+}));
+
+jest.mock('@utils/analytics/hooks/useForkliftAnalytics', () => ({
+  useForkliftAnalytics: () => ({
+    trackEvent: jest.fn(),
+  }),
+}));
+
+jest.mock('src/providers/create/utils/createProvider', () => ({
+  createProvider: (...args: unknown[]) => mockCreateProvider(...args),
+}));
+
+jest.mock('src/providers/create/utils/createProviderSecret', () => ({
+  createProviderSecret: (...args: unknown[]) => mockCreateProviderSecret(...args),
+}));
+
+jest.mock('src/providers/create/utils/patchProviderSecretOwner', () => ({
+  patchProviderSecretOwner: (...args: unknown[]) => mockPatchProviderSecretOwner(...args),
+}));
+
+jest.mock('@utils/hooks/useWatchProjectNames', () => ({
+  __esModule: true,
+  default: () => [['test-namespace'], true],
+}));
+
+jest.mock('../CreateProviderFormContextProvider', () => {
+  const { CreateProviderFormContext } = jest.requireActual('../constants');
+  return {
+    __esModule: true,
+    default: ({ children }: { children: React.ReactNode }) => (
+      <CreateProviderFormContext.Provider
+        value={{
+          providerNames: ['existing-provider'],
+          providerNamesLoaded: true,
+        }}
+      >
+        {children}
+      </CreateProviderFormContext.Provider>
+    ),
+  };
+});
+
+jest.mock('@openshift-console/dynamic-plugin-sdk', () => ({
+  useActiveNamespace: () => ['test-namespace', jest.fn()],
+  useK8sWatchResource: () => [[], true, null],
+  useModal: () => ({ isOpen: false, showModal: jest.fn(), hideModal: jest.fn() }),
+  useAccessReview: () => [true, false],
+  ProjectModel: {
+    apiGroup: 'project.openshift.io',
+    plural: 'projects',
+  },
+}));
+
+jest.mock('@utils/hooks/useDefaultProject', () => ({
+  useDefaultProject: () => 'test-namespace',
+}));
