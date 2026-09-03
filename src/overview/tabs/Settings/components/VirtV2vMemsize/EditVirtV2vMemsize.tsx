a465b4d28d2778db336475c1b46bf99daa1a0b90
Omer Ahronson <oahronso@redhat.com>
2026-09-03 12:19:22 +0300

    Resolves: MTV-6126 | Overview settings numeric inputs silently revert to default on invalid entry
    Signed-off-by: Omer Ahronson <oahronso@redhat.com>

--- src/overview/tabs/Settings/components/VirtV2vMemsize/EditVirtV2vMemsize.tsx
+++ src/overview/tabs/Settings/components/VirtV2vMemsize/EditVirtV2vMemsize.tsx
@@ -1,8 +1,9 @@
-import type { FC } from 'react';
+import { type FC, useState } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 
 import { FormGroupWithHelpText } from '@components/common/FormGroupWithHelpText/FormGroupWithHelpText';
 import { HelpIconPopover } from '@components/common/HelpIconPopover/HelpIconPopover';
+import { getInputValidated } from '@utils/form';
 import { useForkliftTranslation } from '@utils/i18n';
 
 import { defaultValuesMap } from '../../utils/constants';
@@ -14,6 +15,7 @@ import VirtV2vMemsizeHelpContent from './VirtV2vMemsizeHelpContent';
 const EditVirtV2vMemsize: FC = () => {
   const { t } = useForkliftTranslation();
   const { control } = useFormContext<ForkliftSettingsValues>();
+  const [error, setError] = useState<string>();
 
   return (
     <Controller
@@ -22,19 +24,23 @@ const EditVirtV2vMemsize: FC = () => {
       render={({ field: { onChange, value } }) => (
         <FormGroupWithHelpText
           fieldId={SettingsFields.VirtV2vMemsize}
+          helperTextInvalid={error}
           label={t('Conversion appliance memory (MB)')}
           labelHelp={
             <HelpIconPopover header={t('Conversion appliance memory')}>
               <VirtV2vMemsizeHelpContent />
             </HelpIconPopover>
           }
+          validated={getInputValidated(error)}
         >
           <SettingsNumberInput
             defaultValue={Number(defaultValuesMap[SettingsFields.VirtV2vMemsize])}
             onChange={(newValue: number | string) => {
               onChange(Math.max(0, Number(newValue)));
             }}
+            onError={setError}
             testId="settings-virt-v2v-memsize-input"
+            validated={getInputValidated(error)}
             value={value ?? 0}
           />
         </FormGroupWithHelpText>
